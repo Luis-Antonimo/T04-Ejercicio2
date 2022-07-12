ae1f0e649b514076dfc0e2495ea511f298053d02
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 08:34:12 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Diga un nombre de persona:");
+            String nombre = Console.ReadLine ();
+            Console.WriteLine("Diga un nombre de ciudad:");
+            String ciudad = Console.ReadLine ();
+            Console.WriteLine("Hola " + nombre + " bienvenido a " + ciudad);
+        }
+    }
+}
